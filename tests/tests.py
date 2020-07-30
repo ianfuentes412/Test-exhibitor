cd92ecacf7103e96a28faae7a8f6edd753a16f78
ianfuentes412 <ianfuentes412@users.noreply.github.com>
2020-07-29 19:49:23 -0700

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -32,7 +32,7 @@ def test_login_valid(browser):
     assert valid_username in home_page.check_valid_login_usermenu(), "Valid login probably failed or wrong username found after login"
 
 @allure.title('Checking if the video works when clicked')
-def check_home_video(browser):
+def test_home_video(browser):
     home_page = homepageObjects(browser)
     home_page.check_valid_video()
     assert True
