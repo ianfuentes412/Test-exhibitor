cf7e13911f6779c51085aa60218055789622df0e
ianfuentes412 <ianfuentes412@users.noreply.github.com>
2020-07-29 19:46:52 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3  install -r requirements.txt
-python3 -m pytest --alluredir=allure-results tests/loginpage.py -v -s
+python3 -m pytest --alluredir=allure-results tests/tests.py -v -s
